df549be52bf89ad8e9baa9c72c03f05d69205e54
Jeronimo0711 <jeronimomartinez0711@gmail.com>
2026-01-14 11:31:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+
+//ESTRELLAS//
+
 const COLORS = ["#f5f7ff", "#c084fc", "#7aa2ff"]
 
 const generateSpaceLayer = (size, selector, duration) => {
+
     const layer = [];
-    const totalStars = 20;
+    const totalStars = 8;
     for(let i=0; i < totalStars; i++){
         const color = COLORS[Math.floor(Math.random() * COLORS.length)]
         const x = Math.floor(Math.random() * 100);
@@ -14,6 +18,34 @@ const generateSpaceLayer = (size, selector, duration) => {
     container.style.setProperty("--size", size);
     container.style.setProperty("--duration", duration);
 }
-generateSpaceLayer("2px", ".space-1", "17s");
-generateSpaceLayer("4px", ".space-2", "27s");
-generateSpaceLayer("6px", ".space-3", "37s");
+
+generateSpaceLayer("2px", ".space-1", "21.4s");
+generateSpaceLayer("4px", ".space-2", "33.4s");
+generateSpaceLayer("6px", ".space-3", "45.4s");
+
+//ESTRELLAS//
+
+//TODAS LAS FUNCIONES DEL BOTON//
+
+let audioIniciado = false;
+function handleOnClick(event) {
+    event.stopPropagation();
+
+    if (audioIniciado) return;
+    audioIniciado = true;
+
+    alert(' Mis 25 flores para ti :) \n Con MUCHO esfuerzo y amor \n Te quiero mucho 💜'
+
+    );
+    const audio = document.getElementById("audio");
+    audio.volume = 0.6;
+    audio.play();
+    const boton = document.getElementById("miboton");
+    const texto = document.getElementById("mensaje");
+
+    boton.style.display = "none";
+
+    texto.classList.add("activo");
+}
+
+//TODAS LAS FUNCIONES DEL BOTON//
